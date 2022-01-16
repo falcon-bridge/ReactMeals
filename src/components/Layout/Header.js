8200e3abce36ed51eba160fbf0b2f0e130a53ea7
Aayush <falcon.bridge2000@gmail.com>
2022-01-16 16:04:47 +0530

    added fuctionality to manage the visibility of the cart based on a state so that clicking the cart button displays the cart and clicking the close button on the cart and the backdrop hides the cart

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -6,13 +6,13 @@ import mealsImage from "../../assets/meals.jpg";
 
 import classes from "./Header.module.css";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
         {/* <button>Cart</button> */}
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food"></img>
